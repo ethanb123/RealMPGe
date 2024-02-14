d87d5caed6961933f3e48d105b2c485948cdcee8
Ethan Bowman <ethanbowman9@gmail.com>
2024-02-14 14:11:48 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function calculateMPGe() {
+    const gasPrice = parseFloat(document.getElementById('gasPrice').value);
+    const electricPrice = parseFloat(document.getElementById('electricPrice').value);
+    const gasMPG = parseFloat(document.getElementById('gasMPG').value);
+    const electricMiPerkWh = parseFloat(document.getElementById('electricMiPerkWh').value);
+
+    // Calculate the cost to drive an electric car 1 mile
+    const electricCostPerMile = electricPrice / electricMiPerkWh;
+
+    // Calculate the equivalent MPG for the electric car
+    const electricCarMPGe = gasPrice / electricCostPerMile;
+
+    const resultElement = document.getElementById('result');
+    resultElement.innerText = `The electric car has an equivalent MPG of ${electricCarMPGe.toFixed(2)} MPGe`;
+    resultElement.style.display = 'block';
+}
+
+// You can add event listeners here if you prefer to use them instead of inline 'onclick' attribute
