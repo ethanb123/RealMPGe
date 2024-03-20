34a0ef14cc51c560aab850eab76cac7461a573b5
Ethan Bowman <ethanbowman9@gmail.com>
2024-03-20 17:12:23 -0400

    Cleaned up unnecessary scripts

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 document.getElementById('calculate').addEventListener('click', function() {
-    // Read input values
+    // Input values
     var gasPrice = parseFloat(document.getElementById('gasPrice').value);
     var electricPrice = parseFloat(document.getElementById('electricPrice').value);
-    var milesYear = parseFloat(document.getElementById('milesYear').value);
-    var yearsOwnership = parseFloat(document.getElementById('yearsOwnership').value);
     
     var vehicle1Type = document.getElementById('vehicle1Type').value;
-    var vehicle1Efficiency = parseFloat(document.getElementById('vehicle1Efficiency').value);
-    
     var vehicle2Type = document.getElementById('vehicle2Type').value;
+
+    var vehicle1Efficiency = parseFloat(document.getElementById('vehicle1Efficiency').value);
     var vehicle2Efficiency = parseFloat(document.getElementById('vehicle2Efficiency').value);
-    
-    // Calculate electric cost per mile
-    var electricCostPerMile = (vehicle1Type === 'electric') 
-        ? electricPrice / vehicle1Efficiency 
-        : electricPrice / vehicle2Efficiency;
 
-    // Calculate MPG equivalent for electric vehicle
-    var electricCarMPGe = gasPrice / electricCostPerMile;
-
-    // Update the output
-    document.getElementById('mpgEquivalent').innerText = 'Electric Car MPGe: ' + electricCarMPGe.toFixed(2);
+    // Unused inputs
+    var milesYear = parseFloat(document.getElementById('milesYear').value);
+    var yearsOwnership = parseFloat(document.getElementById('yearsOwnership').value);
 
     // Calculate MPGe if the vehicle is electric
     if (vehicle1Type === 'electric') {
@@ -44,7 +35,6 @@ document.getElementById('calculate').addEventListener('click', function() {
         data: [vehicle1Efficiency, vehicle2Efficiency]
     }]);
 
-    
 });
 
 
